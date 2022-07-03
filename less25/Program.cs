e4eae1750c3057caa8182ba3411df1634eaed22a
Dmitry Svyatov <dimka201993@gmail.com>
2022-07-03 18:59:39 +0300

    Задача с возведением в степень

---
+++ less25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите число A = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B = ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result=0;
+int test=1;
+for(int i=1;i<=b;i++)
+{
+    result=test*a;
+    test=result;
+}
+ Console.WriteLine(test);
+
