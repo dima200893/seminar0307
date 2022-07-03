eb90d4e72ba12a064ca6423086c0b6d4ff94cb45
Dmitry Svyatov <dimka201993@gmail.com>
2022-07-03 19:48:28 +0300

    Задача с выодом массива

---
+++ less29/Program.cs
@@ -0,0 +1,24 @@
+﻿// int[]array= new int [8] {1,5,2,7,8,9,5,4};
+int[]array = new int[8];
+for(int i=0;i<array.Length;i++)
+{
+    array[i]=new Random().Next(0,5);
+}
+
+
+
+PrintArray(array);
+
+
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for(int i =0 ; i<array.Length; i++)
+    {
+        Console.Write( array[i]+ " ");
+        
+    }
+    Console.Write("]");   
+}
