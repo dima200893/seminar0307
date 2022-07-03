db52a4ab4af2524fbc044f0a24ff6b5aea25c27e
Dmitry Svyatov <dimka201993@gmail.com>
2022-07-03 19:27:07 +0300

    задача со сложением цифр в числе

---
+++ less27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите число = ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b=0;
+int summ=0;
+while(a>0)
+{
+    b=a%10;
+    a=a/10;    
+    summ+=b;
+}
+Console.WriteLine(summ);
+
+
